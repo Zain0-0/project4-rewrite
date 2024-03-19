505015feb60adf4103e5d4edaeb6c182068bf037
Zain0-0 <122361692+Zain0-0@users.noreply.github.com>
2024-03-19 09:21:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showSideBar(){
+    const sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = "flex"
+}
+function closeSideBar(){
+    const sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = "none"
+}
+function showSideBar2(){
+    const sideBar = document.querySelector(".sideBar2")
+    sideBar.style.display = "flex"
+}
+function closeSideBar2(){
+    const sideBar = document.querySelector(".sideBar2")
+    sideBar.style.display = "none"
+}
